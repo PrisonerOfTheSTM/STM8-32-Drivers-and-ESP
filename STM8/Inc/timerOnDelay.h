7203ced5c567972484975b718de13b0667f3da55
PrisonerOfTheSTM <new_max04@inbox.ru>
2026-09-05 20:45:56 +0700

    Update timerOnDelay.h

--- STM8/Inc/timerOnDelay.h
+++ STM8/Inc/timerOnDelay.h
@@ -1,1 +1,20 @@
+void delay_us(unsigned int a);
+void delay_ms(unsigned int a);
+void set_F_MHz(int f);
 
+typedef struct { //структура, для работы с портами
+ unsigned char ODR;
+ unsigned char IDR;
+ unsigned char DDR;
+ unsigned char CR1;
+ unsigned char CR2;
+}PORT;
+
+//-------------------------указатели для gpio портов--------------
+#define GPIOA ((PORT*)0x5000)
+#define GPIOB ((PORT*)0x5005)
+#define GPIOC ((PORT*)0x500A)
+#define GPIOD ((PORT*)0x500F)
+#define GPIOE ((PORT*)0x5014)
+#define GPIOF ((PORT*)0x5019)
+#define GPIOG ((PORT*)0x501E)
